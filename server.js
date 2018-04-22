a36a2c7e1c650520068b7c046dc74135988bac5c
Chris Belsole <cbelsole@gmail.com>
2018-04-22 12:41:00 -0400

    ditch npm lock for yarn lock and added server logging

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-const express = require('express');
+const express = require('express'),
+      winston = require('winston'),
+      expressWinston = require('express-winston');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    })
+  ],
+  meta: true,
+  msg: "HTTP {{req.method}} {{req.url}}",
+  expressFormat: true,
+  colorize: false,
+  ignoreRoute: function (req, res) { return false; }
+}));
+
 app.get('/api/hello', (req, res) => {
+  // res.status(200);
   res.send({ express: 'Hello From Express' });
 });
 
